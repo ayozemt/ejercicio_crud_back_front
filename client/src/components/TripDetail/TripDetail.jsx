fd375fd5e5a278e1e80e8cb60ec45682ecb296c8
Ayoze Martin <ayozemt@gmail.com>
2023-06-23 21:27:05 +0200

    date format

--- client/src/components/TripDetail/TripDetail.jsx
+++ client/src/components/TripDetail/TripDetail.jsx
@@ -10,8 +10,8 @@ function TripDetail({ title, country, tripImg, startDate, endDate, budget, tasks
         <h5 className="card-title">{title}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{country}</h6>
         <img src={tripImg} alt={title} />
-        <p className="card-text" style={{ color: "black" }}>From: {startDate}</p>
-        <p className="card-text" style={{ color: "black" }}>To: {endDate}</p>
+        <p className="card-text" style={{ color: "black" }}>From: {startDate.split('T')[0]}</p>
+        <p className="card-text" style={{ color: "black" }}>To: {endDate.split('T')[0]}</p>
         <p className="card-text" style={{ color: "black" }}>Budget: {budget}€</p>
         <p className="card-text">{tasks}</p>
         <br />
